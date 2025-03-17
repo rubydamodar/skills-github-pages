fbc0df5ad5241ae49faa2c46875839972d27281e
rubydamodar <rubypoddarr@gmail.com>
2025-03-17 15:04:29 +0530

    Update commit_script.py

--- commit_script.py
+++ commit_script.py
@@ -13,8 +13,8 @@ repo = git.Repo(repo_path)
 file_path = os.path.join(repo_path, "log.txt")
 
 # Define start and end dates (past 1 year)
-start_date = datetime.datetime(2024, 7, 1)
-end_date = datetime.datetime(2024, 11, 1)
+start_date = datetime.datetime(2024, 1, 1)
+end_date = datetime.datetime(2027, 1, 1)
 
 current_date = start_date
 
